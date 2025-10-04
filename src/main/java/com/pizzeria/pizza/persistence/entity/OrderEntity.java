91742db16902c20fefb476ab44cf84a96c401ca0
Sebastian <sebassuarez103@gmail.com>
2025-10-03 22:01:05 -0500

    add entity classes for customer, order, order item, pizza, and application entry point

---
+++ src/main/java/com/pizzeria/pizza/persistence/entity/OrderEntity.java
@@ -0,0 +1,36 @@
+package com.pizzeria.pizza.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "pizza_order")
+public class OrderEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_order", nullable = false)
+    private Integer orderId;
+
+    @Column(name = "idCustomer", nullable = false, length = 15)
+    private String idCustomer;
+
+    @Column(nullable = false, columnDefinition = "DATETIME")
+    private LocalDateTime date;
+
+    @Column(nullable = false, columnDefinition = "Decimal(6,2)")
+    private Double total;
+
+    @Column(nullable = false, columnDefinition = "CHAR(1)")
+    private String method;
+
+    @Column(name = "additional_notes",length = 200)
+    private String additionalNotes;
+}
