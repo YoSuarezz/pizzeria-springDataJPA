91467619d2eaff638d8dff183ec1cfa0ba809064
Sebastian <sebassuarez103@gmail.com>
2025-10-03 22:00:45 -0500

    Initial commit

---
+++ src/test/java/com/pizzeria/pizza/PizzeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pizzeria.pizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
